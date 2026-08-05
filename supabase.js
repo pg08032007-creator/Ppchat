f79128cc23231167402585c159ebcafb4c1ccbc8
Pkzin <pg08032007@gmail.com>
2026-08-05 14:17:27 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,4 @@
 const SUPABASE_URL = "https://hmlzjxmjjsvahzcsktaf.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ouO483LNXe6elR_JH1bWbg_uhpD3X8v";
 
-// Cria o cliente do Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);

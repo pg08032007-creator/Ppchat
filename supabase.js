c4d3bfe0f8b0f8a37688cddb7bd54e44c7e97420
Pkzin <pg08032007@gmail.com>
2026-08-05 14:29:15 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://hmlzjxmjjsvahzcsktaf.supabase.co";
-const SUPABASE_KEY = "sb_publishable_ouO483LNXe6elR_JH1bWbg_uhpD3X8v";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
